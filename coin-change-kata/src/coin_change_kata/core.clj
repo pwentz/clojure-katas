f5cff98aa407239a6a9b246b9bda703a90cfa8e8
pwentz <patrick.w.wentz@gmail.com>
2017-05-18 19:16:51 -0500

    alternate solution for coin_change

--- coin-change-kata/src/coin_change_kata/core.clj
+++ coin-change-kata/src/coin_change_kata/core.clj
@@ -1,9 +1,13 @@
 (ns coin-change-kata.core)
 
+(defn add-coins [cents coins]
+  (loop [acc []
+         cents cents
+         coins coins]
+    (let [coin (first coins)]
+      (cond (empty? coins) acc
+            (<= 0 (- cents coin)) (recur (conj acc coin) (- cents coin) coins)
+            :else (recur acc cents (rest coins))))))
+
 (defn get-change [n-cents]
-  (lazy-seq
-    (cond (<= 0 (- n-cents 25)) (cons 25 (get-change (- n-cents 25)))
-          (<= 0 (- n-cents 10)) (cons 10 (get-change (- n-cents 10)))
-          (<= 0 (- n-cents 5)) (cons 5 (get-change (- n-cents 5)))
-          (<= 0 (- n-cents 1)) (cons 1 (get-change (- n-cents 1)))
-          :else [])))
+  (add-coins n-cents [25 10 5 1]))
